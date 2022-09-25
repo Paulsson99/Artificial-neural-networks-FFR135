259da2c6df404f519a49a199ea9ee764d28e0b73
Simon Paulsson <simon@paulsson.one>
2022-09-25 14:30:29 +0200

    under 12%

--- one-layer-perceptron/main.py
+++ one-layer-perceptron/main.py
@@ -1,7 +1,142 @@
+import os
 import numpy as np
+import pandas as pd
+from tqdm import trange
+import matplotlib.pyplot as plt
 
-from network import OneLayerPerceptron
+from network import Network
 
 
-nn = OneLayerPerceptron(4)
-print(nn.run(np.array([[1], [1]])))
+# Constants
+M = 100
+EPOCHS = 1000
+BATCH_SIZE = 10
+LEARNING_RATE = 0.3
+
+
+def read_data() -> tuple[np.ndarray]:
+    """
+    Read the training and validation data from the csv files
+    """
+    training_data = pd.read_csv('training_set.csv', delimiter=',', header=None).values
+    validation_data = pd.read_csv('validation_set.csv', delimiter=',', header=None).values
+
+    training_x, training_t = training_data[:,:2], training_data[:,-1]
+    validation_x, validation_t = validation_data[:,:2], validation_data[:,-1]
+
+    # Add an extra dimension to the data so it will be 2D when iterating over it
+    return training_x, training_t, validation_x, validation_t
+
+
+def preprocess_data(data: np.ndarray) -> np.ndarray:
+    """
+    Shift and scale the data so the mean becomes 0 and the variance 1
+    Return the required shift and scale factor
+    """
+    shift = np.mean(data, axis=0)
+    variance = np.var(data, axis=0)
+    return shift, 1 / np.sqrt(variance)
+
+
+def plot_data():
+    """
+    The data is 2d so lets plot it to estimate how many hidden neurons are needed
+    """
+    training_x, training_t, validation_x, validation_t = read_data()
+    shift, scale = preprocess_data(training_x)
+    training_x = (training_x - shift) * scale
+    validation_x = (validation_x - shift) * scale
+
+    print(f'Shifting data with {shift}. Scaling with {scale}')
+
+    coord = np.concatenate((training_x, validation_x))
+    target = np.concatenate((training_t, validation_t))
+    colors = ['blue' if t > 0 else 'red' for t in target]
+    plt.scatter(coord[:,0], coord[:,1], c=colors)
+    plt.show()
+
+def update_line(line, x, y):
+    line.set_xdata(np.append(line.get_xdata(), x))
+    line.set_ydata(np.append(line.get_ydata(), y))
+    line.get_figure().canvas.draw()
+
+
+def main():
+    # Preprocessing
+    training_x, training_t, validation_x, validation_t = read_data()
+    shift, scale = preprocess_data(training_x)
+    training_x = (training_x - shift) * scale
+    validation_x = (validation_x - shift) * scale
+
+    print(f'Shifting data with {shift}. Scaling with {scale}')
+    
+
+    # Setup plot
+    plt.ion()
+    fig, ax = plt.subplots()
+    training_loss_line, = ax.plot([], [], '-', label='Training loss')
+    validation_loss_line, = ax.plot([], [], label='Validation loss')
+    training_error_line, = ax.plot([], [], '-', label='Training error')
+    validation_error_line, = ax.plot([], [], label='Validation error')
+    ax.legend()
+    ax.set_xlim(1, EPOCHS)
+    ax.set_ylim(0, 0.5)
+
+    # Setup the network
+    nn = Network([2, M, 1])
+    best_validation_error = np.inf
+    best_w = None
+    best_t = None
+
+    training_samples = training_x.shape[0]
+
+    for epoch in trange(EPOCHS):
+
+        # Train
+        for _ in range(training_samples // BATCH_SIZE):
+            batch_index = np.random.choice(training_samples, BATCH_SIZE, replace=False)
+            nn.backpropagation(training_x[batch_index], training_t[batch_index], lr=LEARNING_RATE)
+
+        # Calculate loss and classification error
+        training_loss = nn.loss(training_x, training_t)
+        validation_loss = nn.loss(validation_x, validation_t)
+        training_error = nn.classification_error(training_x, training_t)
+        validation_error = nn.classification_error(training_x, training_t)
+
+        # Save the weights that gives the smalest validation error
+        if validation_error < best_validation_error:
+            best_validation_error = validation_error
+            best_w = [w.T.copy() for w in nn.weights]
+            best_t = [t.copy() for t in nn.bias]
+
+        # Quit the program if the figure is closed
+        if not plt.fignum_exists(fig.number):
+            print("Training aborted")
+            break
+        
+        # Update the figure
+        update_line(training_error_line, epoch + 1, training_error)
+        update_line(validation_error_line, epoch + 1, validation_error)
+        update_line(training_loss_line, epoch + 1, training_loss)
+        update_line(validation_loss_line, epoch + 1, validation_loss)
+        plt.pause(0.1)
+
+    # Print best result
+    print(f"The best network had a classification error of {best_validation_error:.2%}")
+
+    # Save the weights and bias
+    for i, w in enumerate(best_w):
+        np.savetxt(f"w{i + 1}.csv", w.reshape((M, -1)), delimiter=',')
+    for i, t in enumerate(best_t):
+        np.savetxt(f"t{i + 1}.csv", t[:,np.newaxis], delimiter=',')
+
+    plt.ioff()
+    plt.show()
+
+
+if __name__ == '__main__':
+    # To read data correctly
+    os.chdir(os.path.dirname(__file__))
+
+    # plot_data()
+    main()
--- one-layer-perceptron/network.py
+++ one-layer-perceptron/network.py
@@ -1,62 +1,111 @@
 import numpy as np
 
 
-class OneLayerPerceptron:
+class TangensHyperbolicus:
     """
-    Class for a Neural Network with a single hidden layer, 2 inputs and one output
+    Perform tanh(ax)
     """
 
-    def __init__(self, M: int) -> None:
-        """
-        M: Number of hidden neurons
-        """
-        # Initialze weights and bias
-        self.w1 = np.random.normal(0, 1, size=(M, 2))
-        self.t1 = np.zeros((M, 1))
-        self.w2 = np.random.normal(0, 1, size=(1, M))
-        self.t2 = np.zeros((1, 1))
+    def __init__(self, a: float) -> None:
+        self.a = a
 
-        # Save computations for the backpropagation
-        # b_i is the local field for layer i
-        self.b1 = None
-        self.b2 = None
-        # x is the input
-        self.x = None
-        # V is the hidden layer after activatoin
-        self.V = None
-        # O is the output from the network
-        self.O = None
-        # d_i is the error for layer i
-        self.d1 = None
-        self.d2 = None
+    def __call__(self, x: np.ndarray) -> np.ndarray:
+        return np.tanh(self.a * x)
+
+    def derivative(self, f: np.ndarray) -> np.ndarray:
+        return self.a * (1 - f**2)
+
+
+class Network:
+    """
+    A deep neural network
+    It uses the sgn function for activation
+    """
+
+    def __init__(self, layers: list[int]):
+        self.layers = len(layers) - 1
+        self.weights: list[np.ndarray] = [] 
+        self.bias: list[np.ndarray] = []
+        self.activation = TangensHyperbolicus(1)
+
+        # Variables to save for gradient decent
+        self.V = [None] * (self.layers + 1)
+        self.local_fields: list[np.ndarray] = [None] * self.layers
+
+        for n, m in zip(layers[:-1], layers[1:]):
+            self.weights.append(np.random.random((n, m)) * 2 - 1)
+            self.bias.append(np.zeros((m,)))
+
+    @property
+    def size(self) -> int:
+        """
+        Return the total size of the network (number of tunable parameters)
+        """
+        params = 0
+        for w, b in zip(self.weights, self.bias):
+            params += w.size + b.size
+        return params
 
     def run(self, x: np.ndarray) -> np.ndarray:
         """
-        Run the model with the input x
+        Feed input x to the network. x should be a 2D numpy array with the shape (BATCH_SIZE, N),
+        were N is the number of inputs in a pattern and BATCH_SIZE is the number of patterns. 
+        
+        NOTE: This implies that we are working with row vectors as input so our weight matrix will be transposed!!!
         """
-        self.x = x
-        self.b1 = self.w1 @ self.x - self.t1
-        self.V = np.tanh(self.b1)
-        self.b2 = self.w2 @ self.V - self.t2
-        self.O = np.tanh(self.b2)
+        self.V[0] = x
+        for i in range(self.layers):
+            w, b = self.weights[i], self.bias[i]
+            local_field = x @ w - b
+            self.local_fields[i] = local_field
+            x = self.activation(local_field)
+            self.V[i + 1] = x
+        return x
 
-        return self.O
+    def loss(self, x: np.ndarray, t: np.ndarray) -> float:
+        batch_size = x.shape[0]
+        output = self.run(x)
+        # Reshape the target to match the output
+        t = t.reshape(output.shape)
+        return 0.5 * np.sum((t - output)**2) / batch_size
 
-    def train_batch(self, inputs: list[np.ndarray], targets: list[np.ndarray]) -> float:
+    def classification_error(self, x: np.ndarray, t: np.ndarray) -> float:
+        batch_size = x.shape[0]
+        output = self.run(x)
+        t = t.reshape(output.shape)
+        return np.sum(np.abs(np.sign(output) - t)) / (2 * batch_size)
+
+    def backpropagation(self, x: np.ndarray, t: np.ndarray, lr: float):
         """
-        Train the network on a batch of data and return the total error over the data
+        Perform backpropagation with some training data
         """
-        error = 0
-        for x, t in zip(inputs, targets):
-            output = self.run(x)
-            error += 0.5 * (t - output)**2
+        # Rescale the learning rate for the batch size
 
-    def backpropagation(self) -> None:
-        """
-        Backpropagate through the network to calculate the error for each layer
-        """
-        self.d2 = None
+        if len(x.shape) < 2:
+            x = np.expand_dims(x, axis=0)
 
+        batch_size = x.shape[0]
+        lr /= batch_size
+        
+        outputs = self.run(x)
+        dw = [None] * self.layers
+        db = [None] * self.layers
 
+        # Reshape the target to match the output
+        t = t.reshape(outputs.shape)
+        if len(t.shape) < 2:
+            t = np.expand_dims(t, axis=0)
 
-    
+        error = (t - outputs) * self.activation.derivative(self.V[-1])
+        dw[-1] = lr * np.sum(self.V[-2][:,:,np.newaxis] @ error[:,np.newaxis,:], axis=0)
+        db[-1] = -lr * np.sum(error, axis=0)
+
+        for l in range(self.layers - 1, 0, -1):
+            error = error @ self.weights[l].T * self.activation.derivative(self.V[l])
+            dw[l - 1] = lr * np.sum(self.V[l - 1][:,:,np.newaxis] @ error[:,np.newaxis,:], axis=0)
+            db[l - 1] = -lr * np.sum(error, axis=0)
+
+        # Apply the calculated gradient for the weights and biases
+        for i in range(self.layers):
+            self.weights[i] += dw[i]
+            self.bias[i] += db[i]
