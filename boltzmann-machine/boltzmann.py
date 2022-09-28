87566c2f2229cdb43c1374d38e181859ff617574
Simon Paulsson <simon@paulsson.one>
2022-09-28 15:24:54 +0200

    Boltzmann done

--- boltzmann-machine/boltzmann.py
+++ boltzmann-machine/boltzmann.py
@@ -7,7 +7,7 @@ class BoltzmannMachine:
     """
 
     def __init__(self, visable: int, hidden: int) -> None:
-        self.w = np.random.random((visable, hidden))
+        self.w = np.random.random((hidden, visable))
         self.tv = np.zeros(visable)
         self.th = np.zeros(hidden)
 
@@ -19,14 +19,14 @@ class BoltzmannMachine:
         Initialize the network states
         """
         self.visable = x.copy()
-        self.hidden = self.mcCullochPittsDynamics(self.visable, self.w.T, self.th)
+        self.hidden = self.mcCullochPittsDynamics(self.visable, self.w, self.th)
 
     def update(self):
         """
         Update the network
         """
-        self.visable = self.mcCullochPittsDynamics(self.hidden, self.w, self.tv)
-        self.hidden = self.mcCullochPittsDynamics(self.visable, self.w.T, self.th)
+        self.visable = self.mcCullochPittsDynamics(self.hidden, self.w.T, self.tv)
+        self.hidden = self.mcCullochPittsDynamics(self.visable, self.w, self.th)
 
     @staticmethod
     def mcCullochPittsDynamics(x: np.ndarray, W: np.ndarray, t: np.ndarray) -> np.ndarray:
@@ -41,6 +41,7 @@ class BoltzmannMachine:
         """
         Train the network for k generations on patterns to approximate the distrubution of p_data
         """
+        lr = lr / pattern_batch.shape[0]
         dw = np.zeros_like(self.w)
         dtv = np.zeros_like(self.tv)
         dth = np.zeros_like(self.th)
@@ -49,9 +50,9 @@ class BoltzmannMachine:
             for _ in range(k):
                 self.update()
             
-            bh_0 = self.w.T @ x - self.th
-            bh_k = self.w.T @ self.visable - self.th
-            dw += np.outer(x, np.tanh(bh_0)) - np.outer(self.visable, np.tanh(bh_k))
+            bh_0 = self.w @ x - self.th
+            bh_k = self.w @ self.visable - self.th
+            dw += np.outer(np.tanh(bh_0), x) - np.outer(np.tanh(bh_k), self.visable)
             dtv += -(x - self.visable)
             dth += -(np.tanh(bh_0) - np.tanh(bh_k))
         
--- boltzmann-machine/main.py
+++ boltzmann-machine/main.py
@@ -1,14 +1,21 @@
-from calendar import EPOCH
+from cProfile import label
+from math import floor
 import numpy as np
+import matplotlib.pyplot as plt
+from tqdm import trange
 
 from boltzmann import BoltzmannMachine
 
 
+# Setup matplotlib
+plt.rcParams['text.usetex'] = True
+
 # Constants
-EPOCHS = 1
-BATCH_SIZE = 30
+EPOCHS = 1000
+BATCH_SIZE = 16
 LEARNING_RATE = 0.05
-CD_K = 200
+CD_K = 100
+AVERAGES = 10
 
 
 def train_XOR(M: int) -> float:
@@ -16,6 +23,7 @@ def train_XOR(M: int) -> float:
     Train a boltzmann machine with M hidden neurons on the XOR problem.
     Estimate and return the Kullback-Leibler divergence
     """
+    print(f"Training boltzmann machine with M={M} hidden neurons")
     p_data = np.array(
         [[-1, -1, 1],
         [-1, 1, -1],
@@ -23,9 +31,11 @@ def train_XOR(M: int) -> float:
         [1, 1, 1]]
     )
     bm = BoltzmannMachine(visable=3, hidden=M)
-    for epoch in range(EPOCHS):
+    for _ in trange(EPOCHS):
         batch_index = np.random.randint(4, size=BATCH_SIZE)
         bm.train(p_data[batch_index], k=CD_K, lr=LEARNING_RATE)
+
+    print("Training done. Sampling distrubution")
 
     # Sample the data distrubution for the model
     for _ in range(1000):
@@ -36,12 +46,14 @@ def train_XOR(M: int) -> float:
         bm.update()
         frequenzy_table[hash_state(bm.visable)] += 1
 
-    # Sample the Kullback-Leibler divergence
+    # Sample the Kullback-Leibler divergence (only need the XOR data as all other have probability zero -> vanish in D_KL sum)
     p_model = [frequenzy_table[hash_state(p)] / 10_000 for p in p_data]
 
-    print(p_model)
+    print(f"Distrubution for the valid inputs is: {p_model}")
 
-    D_KL = sum([1/4 * (np.log(1/4) - np.log(p_f)) for p_f in p_model])
+    D_KL = np.sum([1/4 * (np.log(1/4) - np.log(p_f)) for p_f in p_model])
+
+    print(f"Kullback-Leibler divergence for M={M} is: {D_KL}")
 
     return D_KL
 
@@ -57,11 +69,35 @@ def hash_state(state: np.ndarray):
     return h
 
 
-def main():
-    M = [1, 2, 4, 8]
+def upperKullbackLeiblerDivergence(N: int, M: np.ndarray) -> np.ndarray:
+    """
+    Calculate the thoretical upper bound for the Kullback-Leibler divergence
+    """
+    d_kl = np.zeros_like(M, dtype=float)
+    mask = M < 2**(N - 1) - 1
+    m = M[mask] + 1.0
+    print(m)
+    m = N - np.floor(np.log2(m)) - m / (2**np.log2(m))
+    print(m)
+    d_kl[mask] = np.log(2) * m
+    print(m)
+    print(d_kl)
+    return d_kl
 
-    for m in M:
-        KullbackLeibler = train_XOR(m)
+
+def main():
+    M = np.array([1, 2, 4, 8])
+
+    KullbackLeibler = np.zeros_like(M, dtype=float)
+    for i in range(AVERAGES):
+        KullbackLeibler += np.array([train_XOR(m) for m in M]) / AVERAGES
+
+    plt.plot(M, KullbackLeibler, label="Boltzmann Machine")
+    plt.plot(M, upperKullbackLeiblerDivergence(N=3, M=M), label="Theoretical upper bound")
+    plt.legend()
+    plt.xlabel(r"$M$")
+    plt.ylabel(r"$D_\textrm{KL}$")
+    plt.show()
 
 
 if __name__ == '__main__':
