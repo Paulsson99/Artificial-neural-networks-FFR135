69a7d68cc096a4b9073580348dce31021ed6a5f7
Simon Paulsson <simon@paulsson.one>
2022-09-30 15:09:54 +0200

    boltzmann done

--- boltzmann-machine/main.py
+++ boltzmann-machine/main.py
@@ -1,3 +1,4 @@
+from cProfile import label
 import numpy as np
 import matplotlib.pyplot as plt
 from tqdm import trange
@@ -9,11 +10,12 @@ from boltzmann import BoltzmannMachine
 plt.rcParams['text.usetex'] = True
 
 # Constants
-EPOCHS = 1000
-BATCH_SIZE = 16
-LEARNING_RATE = 0.05
+EPOCHS = 500
+BATCH_SIZE = 32
+LEARNING_RATE = 0.1
 CD_K = 100
-AVERAGES = 10
+AVERAGES = 20
+SAMPLING_STEPS = 10_000
 
 
 def train_XOR(M: int) -> float:
@@ -21,7 +23,6 @@ def train_XOR(M: int) -> float:
     Train a boltzmann machine with M hidden neurons on the XOR problem.
     Estimate and return the Kullback-Leibler divergence
     """
-    print(f"Training boltzmann machine with M={M} hidden neurons")
     p_data = np.array(
         [[-1, -1, 1],
         [-1, 1, -1],
@@ -29,30 +30,21 @@ def train_XOR(M: int) -> float:
         [1, 1, 1]]
     )
     bm = BoltzmannMachine(visable=3, hidden=M)
-    for _ in trange(EPOCHS):
+    for _ in trange(EPOCHS, desc=f"Training boltzmann machine with M={M} hidden neurons", leave=False):
         batch_index = np.random.randint(4, size=BATCH_SIZE)
         bm.train(p_data[batch_index], k=CD_K, lr=LEARNING_RATE)
 
-    print("Training done. Sampling distrubution")
-
     # Sample the data distrubution for the model
+    frequenzy_table = {i: 0 for i in range(8)}
     for _ in range(1000):
         bm.update() # Remove initial transient
-
-    frequenzy_table = {i: 0 for i in range(8)}
-    for _ in range(10_000):
+    for _ in range(SAMPLING_STEPS):
         bm.update()
         frequenzy_table[hash_state(bm.visable)] += 1
 
     # Sample the Kullback-Leibler divergence (only need the XOR data as all other have probability zero -> vanish in D_KL sum)
-    p_model = [frequenzy_table[hash_state(p)] / 10_000 for p in p_data]
-
-    print(f"Distrubution for the valid inputs is: {p_model}")
-
+    p_model = [frequenzy_table[hash_state(p)] / SAMPLING_STEPS for p in p_data]
     D_KL = np.sum([1/4 * (np.log(1/4) - np.log(p_f)) for p_f in p_model])
-
-    print(f"Kullback-Leibler divergence for M={M} is: {D_KL}")
-
     return D_KL
 
 
@@ -74,23 +66,23 @@ def upperKullbackLeiblerDivergence(N: int, M: np.ndarray) -> np.ndarray:
     d_kl = np.zeros_like(M, dtype=float)
     mask = M < 2**(N - 1) - 1
     m = M[mask] + 1.0
-    print(m)
     m = N - np.floor(np.log2(m)) - m / (2**np.floor(np.log2(m)))
-    print(m)
     d_kl[mask] = np.log(2) * m
-    print(m)
-    print(d_kl)
     return d_kl
 
 
 def main():
     M = np.array([1, 2, 4, 8])
 
-    KullbackLeibler = np.zeros_like(M, dtype=float)
-    for i in range(AVERAGES):
-        KullbackLeibler += np.array([train_XOR(m) for m in M]) / AVERAGES
+    kullback_leibler_samples = np.zeros((AVERAGES, 4))
+    for i in trange(AVERAGES, desc="Calculating the average of the Kullback-Leibler divergence"):
+        kullback_leibler_samples[i,:] = np.array([train_XOR(m) for m in M])
 
-    plt.plot(M, KullbackLeibler, label="Boltzmann Machine")
+    kullback_leibler_mean = np.mean(kullback_leibler_samples, axis=0)
+
+    plt.scatter(np.repeat(M, AVERAGES, axis=0), kullback_leibler_samples, marker='x',
+     label="Samples from the trained Boltzmann Machine")
+    plt.plot(M, kullback_leibler_mean, label=f"Average over {AVERAGES} samples")
     plt.plot(M, upperKullbackLeiblerDivergence(N=3, M=M), label="Theoretical upper bound")
     plt.legend()
     plt.xlabel(r"$M$")
